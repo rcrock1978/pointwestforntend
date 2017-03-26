4fbb137be57d4e90883aa244792f565d82686f49
Raymund Caringal <rcrock1978@gmail.com>
2017-03-26 23:54:46 +0800

    Correct the packages

--- app.js
+++ app.js
@@ -25,7 +25,7 @@ app.get('*', (req, res) => {
 });
 
 app.listen(port, () => {
-   console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+    console.log('Server started on port ' + port);
 });
 
 
